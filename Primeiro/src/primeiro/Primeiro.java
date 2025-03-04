3449241503c6f92809a6d6c0b88e3c296acd3f56
Mutasba <mamadumutarobaprogramador@gmail.com>
2025-03-04 15:23:09 -0300

    removendo linhas

--- Primeiro/src/primeiro/Primeiro.java
+++ Primeiro/src/primeiro/Primeiro.java
@@ -15,18 +15,7 @@ public class Primeiro {
      */
     public static void main(String[] args) {
         System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
+
         
     }
     
