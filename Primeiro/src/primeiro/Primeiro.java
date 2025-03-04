6749473c30e137200cc1e11085ca40f9646ebbf4
Mutasba <mamadumutarobaprogramador@gmail.com>
2025-03-04 15:12:58 -0300

    teste hola mundo

--- Primeiro/src/primeiro/Primeiro.java
+++ Primeiro/src/primeiro/Primeiro.java
@@ -15,6 +15,19 @@ public class Primeiro {
      */
     public static void main(String[] args) {
         System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        
     }
     
 }
